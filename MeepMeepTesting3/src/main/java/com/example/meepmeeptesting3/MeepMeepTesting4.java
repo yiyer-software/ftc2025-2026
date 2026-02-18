affeabc141faa0850e59dd9d8923e627de719964
Om <odagadgunde27@mhrd.org>
2026-02-18 07:39:05 -0500

    eeeee

---
+++ MeepMeepTesting3/src/main/java/com/example/meepmeeptesting3/MeepMeepTesting4.java
@@ -0,0 +1,61 @@
+package com.example.meepmeeptesting3;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting4 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(
+                        60, 60,
+                        Math.toRadians(180),
+                        Math.toRadians(180),
+                        16.5
+                )
+                .build();
+
+        myBot.runAction(
+                myBot.getDrive().actionBuilder(
+                                // Start reflected across X axis
+                                new Pose2d(-52, 48, Math.toRadians(135))
+                        )
+                        .strafeTo(new Vector2d(-36, 34))
+                        .strafeTo(new Vector2d(-12, 0))
+
+                        .turnTo(Math.toRadians(90))
+                        .strafeTo(new Vector2d(-12, 50))
+
+                        .turnTo(Math.toRadians(135))
+
+                        .splineToConstantHeading(new Vector2d(-24, 45), Math.toRadians(180))
+                        .splineToConstantHeading(new Vector2d(-45, 34), Math.toRadians(0))
+
+                        .splineToLinearHeading(new Pose2d(-34, 30, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(12, 30, Math.toRadians(90)), Math.toRadians(0))
+
+                        .strafeTo(new Vector2d(12, 50))
+                        .strafeTo(new Vector2d(12, 44))
+
+                        .splineToLinearHeading(new Pose2d(-30, 32, Math.toRadians(135)), Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(36, 20, Math.toRadians(90)), Math.toRadians(0))
+
+                        .strafeTo(new Vector2d(36, 50))
+
+                        .splineToLinearHeading(new Pose2d(22, 50, Math.toRadians(90)), Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(-34, 30, Math.toRadians(135)), Math.toRadians(0))
+
+                        .build()
+        );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_FINAL_Blue_Back_SIMPLE.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.Action;
+@Autonomous(name = "Auto_FINAL_Blue_Back_SIMPLE", group = "Auton")
+public class Auto_FINAL_Blue_Back_SIMPLE extends LinearOpMode {
+
+    private DcMotorEx frontLeft, frontRight, backLeft, backRight;
+    private DcMotor intakeMotor;
+    private DcMotorEx launcherLeft, launcherRight;
+    private Servo liftLeft, liftRight;
+
+    private double FLAP_DOWN_POS = 0;
+    private static final double TICKS_PER_REV = 28.0;
+    private static final double MAX_DRIVE_RPM = 2600;
+    private static final double DRIVE_TICKS_PER_SEC = (MAX_DRIVE_RPM / 60.0) * TICKS_PER_REV;
+    private double FLAP_UP_POS = 0.5;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        initHardware();
+
+        liftLeft.setPosition(0);
+        liftRight.setPosition(1);
+        waitForStart();
+
+        while (opModeIsActive()){
+            rotateLeft(0.78);
+            sleep(700);
+            rotateLeft(0);
+            setDrivePower(1);
+            sleep(1100);
+            setDrivePower(0);
+            stop();
+
+        }
+    }
+
+    private void rotateLeft(double power) {
+        frontLeft.setPower(-power);
+        backLeft.setPower(-power);
+        frontRight.setPower(power);
+        backRight.setPower(power);
+    }
+    private void setDrivePower(double power) {
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+    }
+
+    private void initHardware() {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "leftFront");
+        frontRight = hardwareMap.get(DcMotorEx.class, "rightFront");
+        backLeft = hardwareMap.get(DcMotorEx.class, "leftBack");
+        backRight = hardwareMap.get(DcMotorEx.class, "rightBack");
+
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+
+        launcherLeft = hardwareMap.get(DcMotorEx.class, "launcherLeft");
+        launcherRight = hardwareMap.get(DcMotorEx.class, "launcherRight");
+
+        liftLeft = hardwareMap.get(Servo.class, "liftServoLeft");
+        liftRight = hardwareMap.get(Servo.class, "liftServoRight");
+
+        // ==== Flip directions because front is now back ====
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        launcherLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        launcherRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        launcherLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcherRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+}
